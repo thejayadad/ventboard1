ca10426022f3b8187c4dfaddbe8acde1005125a6
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-01-07 12:03:56 -0500

    basic-router-dom-setup

--- ventboard/src/App.js
+++ ventboard/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import MessageListing from './pages/MessageListing';
 import MessagePage from './pages/MessagePage';
@@ -9,9 +10,19 @@ import NotFound from './pages/NotFound';
 function App() {
   return (
     <div className='main'>
-        <h2>Tha Vent Board</h2>
-      
+
+    <Router>
+    <h2>Tha Vent Board</h2>
+        <Routes>
+        <Route exact path='/' element={<HomePage/>} />
+        <Route exact path='/messagelisting' element={<MessageListing/>} />
+        <Route exact path='/mission' element={<Mission/>} />
+        <Route exact path='/messagepage/:messageId' element={<MessagePage/>} />
+        <Route exact path='/messagepage' element={<NotFound/>} />
+        </Routes>
+    </Router>
     </div>
+
   );
 }
 
